6f5ceb6e9ed8026ee02acb73c6ec4923760d6e04
Filip Urzoń <filip.urzon2003@gmail.com>
2023-09-18 20:20:47 +0200

    Mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("Database connected")
+})
+
 const app = express()
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
