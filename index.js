3861ec8c59f893d64f72990bb38070a71c2d01e4
Filip Urzoń <filip.urzon2003@gmail.com>
2023-09-20 03:25:51 +0200

    Added get:campgrounds/new post:campgrounds/ , views/campgrounds/new.ejs

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home', { name: 'YELPCAMP' })
@@ -28,6 +29,15 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new')
+})
 
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params
