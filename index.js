465f18cb9865d9971a820b42dd4573230f98f583
Filip Urzoń <filip.urzon2003@gmail.com>
2023-10-07 23:09:43 +0200

    Imported ejs-mate

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -15,7 +16,7 @@ db.once("open", () => {
 
 const app = express()
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
