70b995b563517b470737248da17547bb29e99c02
Filip Urzoń <filip.urzon2003@gmail.com>
2024-02-20 22:38:05 +0100

    Added error.ejs template

--- index.js
+++ index.js
@@ -77,8 +77,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { message = "Something went wrong", statusCode = 500 } = err
-    res.status(statusCode).send(message)
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = "Oh No, Something Went wrong!"
+    res.status(statusCode).render('partials/error', { err })
 })
 
 app.listen(3000, () => {
