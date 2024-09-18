826295ab291144956cdef5fe0e9f23fcc4b6602a
Filip Urzoń <filip.urzon2003@gmail.com>
2024-09-18 02:29:27 +0200

    add: reviews router

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 
-const { campgroundSchema, reviewSchema } = require('./schemas')
-const Campground = require('./models/campground')
-const Review = require('./models/review')
-const catchAsync = require('./utils/catchAsync')
 const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError')
 const campgrounds = require('./routes/campgrounds')
-
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
@@ -29,40 +25,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-
-
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
-
 app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('home', { name: 'YELPCAMP' })
 })
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError("Page Not Found", 404))
