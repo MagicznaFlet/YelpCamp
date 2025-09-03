4830dfb3fed3e54acef103b3b9b342df9491b144
Filip Urzoń <filip.urzon2003@gmail.com>
2025-09-03 23:52:02 +0200

    Login redirects to pre login page

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,26 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
+const { storeReturnTo } = require('../middleware');
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
+
 
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res,next) => {
     try {
         const { username, email, password } = req.body
         const newUser = new User({ email, username })
         const registratedUser = await User.register(newUser, password)
-        req.flash('success', 'Successfully created user!')
-        res.redirect('/campgrounds')
+        req.login(registratedUser,err =>{
+            if(err) return next(err)
+            req.flash('success', 'Wellcome to Yelp Camp!')
+            res.redirect('/campgrounds')
+        })
+        
     } catch (e) {
         req.flash('error', e.message)
         res.redirect('/register')
@@ -26,9 +32,10 @@ router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo,passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!')
-    res.redirect('/campgrounds')
+    const redirectUrl = res.locals.returnTo || '/campgrounds'
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req, res, next) => {
